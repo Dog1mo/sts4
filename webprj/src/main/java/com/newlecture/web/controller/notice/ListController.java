fc9ef9334c3a14a34854fd830e0db5ae7bf1b751
Dog1mo <dpodpo4321@gmail.com>
2022-04-30 23:57:47 +0900

    Spring MVC.

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -13,23 +15,16 @@ import com.newlecture.web.service.NoticeService;
 
 public class ListController implements Controller{
 
+	@Autowired
 	private NoticeService noticeService;
-	
-	
-	
-	public void setNoticeService(NoticeService noticeService) {
-		this.noticeService = noticeService;
-	}
-
-
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		ModelAndView mv = new ModelAndView("notice.list");
 		//mv.setViewName("/WEB-INF/view/notice/list.jsp");
-		List<Notice> list = noticeService.getList(1, "title", "");
-		mv.addObject("list",list);
+		List<Notice> list = noticeService.getList(1, "TITLE", "");
+		mv.addObject("list", list);
 		return mv;
 	}
 
